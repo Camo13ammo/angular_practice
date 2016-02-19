d391ad427d59bed4ee488c20403630078345bfb3
Vincent Leombruno <vleombruno42@gmail.com>
2016-02-18 20:06:46 -0500

    finished practice

--- browser/js/app.js
+++ browser/js/app.js
@@ -9,19 +9,38 @@ app.controller('MainCtrl', function($scope) {
   //Creates an item object and adds it to the items array
   $scope.addItem = function() {
     var listItem = {
-      //item data here
+      todo:$scope.todo,
+      completed: false
     }
 
-    $scope.items.push(listItem)
+    $scope.items.push(listItem);
+    $scope.todo = "";
   }
-
 })
 
 app.directive('listItem', function() {
   return {
-
+    restrict: "E",
+    scope:{
+      items: "=",
+      item: "="
+    },
+    templateUrl: '/templates/listItem.html',
+    link: function(scope){
+      scope.complete = function(){
+        scope.item.completed = !scope.item.completed;
+      }
+      scope.button = function(){
+        if(!scope.item.completed) return "Complete it!"
+        return "Put it back"
+      }
+      scope.remove = function(){
+        utilsModule.remove(scope.items, scope.item);
+      }
+    }
   }
 })
+
 
 //This module contains some helpful methods
 var utilsModule = {
